9b24e974c86b8b6cb394ba80f26464cc77825a70
severejetlag <nhl1013@gmail.com>
2017-09-28 19:41:09 -0700

    adding stopwatch logic

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,17 @@
 console.log("Sanity Check: JS is working!");
 
+var startTime;
+var endTime;
+var totalTime = 0; 
 $(document).ready(function(){
-
+	$(window).on('keypress',function(){
+		if(startTime && startTime !== 0){
+			endTime = Date.now();
+			totalTime = endTime - startTime;
+			$('#total-time').text(totalTime/1000);
+			startTime = 0;
+		}else {
+			startTime = Date.now();
+		}
+	})
 })
