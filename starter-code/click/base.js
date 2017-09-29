c1afdb92af2f6c6076ba1425eef21876f257a7b1
severejetlag <nhl1013@gmail.com>
2017-09-28 18:57:34 -0700

    added click logic

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+	$("#imperatives span").on('click',function(event){
+		var insertText = $(this).text();
+		$('ul').append(`<li>${insertText} at ${Date.now()}</li>`)
+	})
 })
