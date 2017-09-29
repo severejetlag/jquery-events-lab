fd19ce2361767fd45aae9db1919ab659418aad23
severejetlag <nhl1013@gmail.com>
2017-09-28 18:40:50 -0700

    Add logic for adding

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('input.add').on('change',function(){
+  	var leftVal = $("#left").val();
+    var rightVal = $("#right").val();
+    var num1 = parseInt(leftVal) || 0;
+    var num2 = parseInt(rightVal) || 0;
+    var total = num1 + num2;
+    $("#total").val(total);
+  })
+
 })
